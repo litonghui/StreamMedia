35fbc0544b2b13de4a257fdabb5e20449668e854
lth <952650874@qq.com>
2016-05-13 19:27:40 +0800

    调试

--- app/src/main/java/com/android/streammedia/activity/MainActivity.java
+++ app/src/main/java/com/android/streammedia/activity/MainActivity.java
@@ -1,16 +1,17 @@
 package com.android.streammedia.activity;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.android.streammedia.R;
+import com.android.streammedia.tools.Utils;
 import com.android.streammedia.view.ActionMenu;
+import com.android.streammedia.view.MenuItem;
+import com.android.streammedia.view.MenuItemView;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
@@ -18,14 +19,20 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     private LinearLayout mLayout;
 
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLayout  = (LinearLayout) findViewById(R.id.ly_main);
+        init();
         initView();
     }
-    private void initView(){
+    private void init(){
+        mContext = this;
+        mLayout  = (LinearLayout) findViewById(R.id.ly_main);
+    }
+    /*private void initView(){
         mActionMenu = new ActionMenu.Builder(this)
                 .addMenuItem(R.color.photo, R.mipmap.ic_messaging_posttype_photo, "Photo", R.color.text_color,this)
                 .addMenuItem(R.color.chat, R.mipmap.ic_messaging_posttype_chat, "Chat", R.color.text_color,this)
@@ -37,8 +44,16 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 .build();
         LinearLayout.LayoutParams labelLp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         labelLp.gravity = Gravity.CENTER_HORIZONTAL;
-        mLayout.addView(mActionMenu,labelLp);
+        mLayout.addView(mActionMenu, labelLp);
+    }*/
+    private void initView(){
+        MenuItem item = new MenuItem(R.color.photo, R.mipmap.ic_messaging_posttype_photo, "Photo", R.color.text_color,this) ;
+        MenuItemView menuItemView = new MenuItemView(mContext, item);
+        menuItemView.setLayoutParams(Utils.createWrapParams());
+        menuItemView.setOnClickListener(item.getOnClickListener());
+        mLayout.addView(menuItemView);
     }
+
 
     @Override
     public void onClick(View v) {
